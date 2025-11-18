868719b2d2bba78e64c6b87ecf077623a704ecb4
kavyan555 <kavyashreen2005@gmail.com>
2025-11-18 20:44:11 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,18 @@
+public class arrays {
+    public static void main(String[] args) {
+        /*classroom of 500 students ypu have to store 
+         * you have two options
+         * 1.create 500 variuable 
+         * 2.use arrays (recommended) 
+         */
+        int [] marks = new int[5];
+        marks[0]= 100;
+        marks[1] = 98;
+        marks[2 ]=78;
+        marks[3]=89;
+        System.out.println(marks[3]);
+        //marks[5]=96; throws an error
+
+    }
+    
+}
