2443fab9eee851c9ccabaecdceb3cce9a799bc21
kavyan555 <kavyashreen2005@gmail.com>
2025-11-18 20:41:12 +0530

    Name-Copy

---
+++ Name-Copy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Name {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = s.nextLine();
+        System.out.println("Hello"+  name);
+        System.out.println("have a good day");
+        
+    }
+    
+}
